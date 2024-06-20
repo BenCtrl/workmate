e6ebe721222c7e5499394c0038888be2a12bfff8
Ben Cartwright <bcbenc5@hotmail.co.uk>
2024-06-20 15:48:55 +0100

    Create baseline files & tidy up boilerplate for project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <div>Workmate</div>
+      <div>This is a test paragraph to demonstrate the provisional font of this app.</div>
+    </>
+  )
+}
+
+export default App
