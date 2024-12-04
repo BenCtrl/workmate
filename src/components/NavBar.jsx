abf9e5a2332af16f940fd10467b50aad9c6894fc
Ben Cartwright <bcbenc5@hotmail.co.uk>
2024-12-04 19:03:00 +0000

    Changed app title & logo because why not :)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { HiCodeBracket} from "react-icons/hi2";
+import { ImCheckmark } from "react-icons/im";
 import '../styling/navbar.css'
 
 const NavBar = () => {
@@ -8,7 +9,13 @@ const NavBar = () => {
   return (
     <div id="navbar">
       <div className="app-identifier">
-        <HiCodeBracket style={{marginRight: '0.3rem'}}/><i className='app-name'>workmate</i>
+        <i className='app-name'>workmate</i>
+        <ImCheckmark style={{
+          position: 'relative',
+          marginLeft: '0.4rem',
+          top: '0',
+          color: '#3d3d3d87'
+        }} />
       </div>
 
       <i style={{fontSize: "1rem", color: "#bbbbbb"}}>Your friend for work :)</i>
