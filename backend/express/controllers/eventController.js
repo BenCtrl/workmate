f1c64892d9d663febca3d5752817f56d7ad02bdc
Ben Cartwright <bcbenc5@hotmail.co.uk>
2025-02-04 16:16:18 +0000

    Implement basic logging into eventController file

--- backend/express/controllers/eventController.js
+++ backend/express/controllers/eventController.js
@@ -1,57 +1,70 @@
 import Database from "better-sqlite3";
-const database = new Database('workmate.db', {});
+import { getLogger } from "../../../src/features/logging/logging.js";
 
-export const getEvents = (request, response, nextMiddleWare) => {
+const database = new Database('workmate.db', {});
+const apiLogger = getLogger('workmate_api');
+
+export const getEvents = (request, response, errorHandlerMiddleware) => {
   try {
-    response.json(database.prepare('SELECT * FROM events;').all());
+    const events = database.prepare('SELECT * FROM events;').all();
+    apiLogger.info(`Successfully retrieved all '${events.length}' events`);
+
+    response.status(200).json(events);
   } catch (error) {
-    return nextMiddleWare(new Error(error));
+    return errorHandlerMiddleware(new Error(error));
   }
 }
 
-export const getEvent = (request, response, nextMiddleWare) => {
+export const getEvent = (request, response, errorHandlerMiddleware) => {
   try {
     const eventID = request.params.id;
-    response.status(200).json(database.prepare('SELECT * FROM events WHERE id = ?').get(eventID));
+    const event = database.prepare('SELECT * FROM events WHERE id = ?').get(eventID);
+    apiLogger.info(`Successfully retrieved event with ID '${eventID}'`);
+
+    response.status(200).json(event);
   } catch (error) {
-    return nextMiddleWare(new Error(error));
+    return errorHandlerMiddleware(new Error(error));
   }
 }
 
-export const createEvent = (request, response, nextMiddleWare) => {
+export const createEvent = (request, response, errorHandlerMiddleware) => {
   try {
     console.log(request.body);
     const eventTitle = request.body.title;
     const eventTimestamp = request.body.event_timestamp;
 
-    const preparedStatement = database.prepare('INSERT INTO events (title, event_timestamp) VALUES (?, ?) RETURNING id');
+    const event = database.prepare('INSERT INTO events (title, event_timestamp) VALUES (?, ?) RETURNING id').get(eventTitle, eventTimestamp);
+    apiLogger.info(`Successfully created new event with ID '${event.id}'`);
 
-    response.status(201).json(preparedStatement.get(eventTitle, eventTimestamp));
+    response.status(201).json(event);
   } catch (error) {
-    return nextMiddleWare(new Error(error));
+    return errorHandlerMiddleware(new Error(error));
   }
 }
 
-export const updateEvent = (request, response, nextMiddleWare) => {
+export const updateEvent = (request, response, errorHandlerMiddleware) => {
   try {
     const eventID = request.params.id;
     const eventTitle = request.body.title;
     const eventTimestamp = request.body.event_timestamp;
 
-    const preparedStatement = database.prepare('UPDATE events SET title = ?, event_timestamp = ? WHERE id = ?');
-    response.status(204).json(preparedStatement.run(eventTitle, eventTimestamp, eventID));
+    const updatedEvent = database.prepare('UPDATE events SET title = ?, event_timestamp = ? WHERE id = ?').run(eventTitle, eventTimestamp, eventID);
+    apiLogger.info(`Successfully updated event with ID '${eventID}'`);
+
+    response.status(204).json(updatedEvent);
   } catch (error) {
-    return nextMiddleWare(new Error(error));
+    return errorHandlerMiddleware(new Error(error));
   }
 }
 
-export const deleteEvent = (request, response, nextMiddleWare) => {
+export const deleteEvent = (request, response, errorHandlerMiddleware) => {
   try {
     const eventID = request.params.id;
-    const preparedStatement = database.prepare('DELETE FROM events WHERE id = ?');
+    const deletedEvent = database.prepare('DELETE FROM events WHERE id = ?').run(eventID);
+    apiLogger.info(`Successfully deleted event with ID '${eventID}'`);
 
-    response.status(200).json(preparedStatement.run(eventID));
+    response.status(200).json(deletedEvent);
   } catch (error) {
-    return nextMiddleWare(new Error(error));
+    return errorHandlerMiddleware(new Error(error));
   }
 }
