a51fcd35b78e9d96904cf1f69928d63ea9f99db6
Ben Cartwright <bcbenc5@hotmail.co.uk>
2025-02-04 17:14:28 +0000

    Change log level in api middleware logger to from INFO to DEBUG

--- backend/express/middleware/logger.js
+++ backend/express/middleware/logger.js
@@ -3,7 +3,7 @@ import { getLogger } from "../../../src/features/logging/logging.js";
 const apiLogger = getLogger('workmate_api');
 
 const logger = (request, response, nextMiddleware) => {
-  apiLogger.info(`Incoming '${request.method}' request made to '${request.protocol}://${request.get('host')}${request.originalUrl}'`);
+  apiLogger.debug(`Incoming '${request.method}' request made to '${request.protocol}://${request.get('host')}${request.originalUrl}'`);
   nextMiddleware();
 }
 
