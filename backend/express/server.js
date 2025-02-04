78bec95c7db9e061ded8fcbb8d1553d481ef1617
Ben Cartwright <bcbenc5@hotmail.co.uk>
2025-02-04 16:21:14 +0000

    Implement basic logging into server file for reading & updating of app_settings.json

--- backend/express/server.js
+++ backend/express/server.js
@@ -7,11 +7,13 @@ import eventRoutes from './routes/events.js'
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
 import fs from 'fs';
+import { getLogger } from '../../src/features/logging/logging.js';
 
 const server = express();
 const port = 8080;
 
 const database = new Database('workmate.db', {});
+const apiLogger = getLogger('workmate_api');
 
 server.use(express.json());
 server.use(express.urlencoded({extended: false}));
@@ -22,26 +24,27 @@ server.use('/api/stickynote_groups', noteGroups);
 server.use('/api/pages', pageRoutes);
 server.use('/api/events', eventRoutes);
 
-server.get('/api/app_settings', (request, response) => {
+server.get('/api/app_settings', (request, response, errorHandlerMiddleware) => {
   fs.readFile('./src/app_settings.json', 'utf8', (error, data) => {
     if (error) {
-      response.status(500).send(error);
-      return;
+      return errorHandlerMiddleware(new Error(error));
     }
 
-    response.send(JSON.parse(data));
+    apiLogger.info('Successfully found and read app_settings file');
+    response.status(200).send(JSON.parse(data));
   });
 })
 
-server.put('/api/app_settings', (request, response) => {
+server.put('/api/app_settings', (request, response, errorHandlerMiddleware) => {
   const app_settings = request.body;
 
   fs.writeFile('./src/app_settings.json', JSON.stringify(app_settings), 'utf8', (error) => {
     if (error) {
-      response.status(500).send(error);
-      return;
+      return errorHandlerMiddleware(new Error(error));
     }
-    response.send('Data updated successfully');
+
+    apiLogger.info('Successfully found and updated app_settings file')
+    response.status(204).send('Data updated successfully');
   });
 });
 
