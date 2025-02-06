a4d06321894c5def2d8d659e7b9b0d1f29064603
Ben Cartwright <bcbenc5@hotmail.co.uk>
2025-02-06 12:57:21 +0000

    Install & configure @tauri-apps/plugin-sql module so REACT codebase can interact with sqlite database through tauri window APIs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,36 @@
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+  let migrations = vec![
+    Migration {
+      version: 1,
+      description: "create notes table",
+      sql: "CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY, content TEXT NOT NULL, completed INTEGER NOT NULL, created_timestamp INTEGER DEFAULT(unixepoch('subsec') * 1000), edited_timestamp INTEGER DEFAULT(unixepoch('subsec') * 1000), group_id INTEGER NOT NULL, FOREIGN KEY (group_id) REFERENCES note_groups (id));",
+      kind: MigrationKind::Up,
+    },
+    Migration {
+      version: 2,
+      description: "create note groups table",
+      sql: "CREATE TABLE IF NOT EXISTS note_groups (id INTEGER PRIMARY KEY, title TEXT NOT NULL, color TEXT NOT NULL);",
+      kind: MigrationKind::Up,
+    },
+    Migration {
+      version: 3,
+      description: "create pages table",
+      sql: "CREATE TABLE IF NOT EXISTS pages (id INTEGER PRIMARY KEY, title TEXT NOT NULL, page_content TEXT NOT NULL, created_timestamp INTEGER DEFAULT(unixepoch('subsec') * 1000) NOT NULL, edited_timestamp INTEGER DEFAULT(unixepoch('subsec') * 1000));",
+      kind: MigrationKind::Up,
+    },
+    Migration {
+      version: 4,
+      description: "create events groups table",
+      sql: "CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY, title TEXT NOT NULL, event_timestamp INTEGER DEFAULT(unixepoch('subsec') * 1000) NOT NULL);",
+      kind: MigrationKind::Up,
+    }
+  ];
+
   tauri::Builder::default()
+    .plugin(tauri_plugin_sql::Builder::new().add_migrations("sqlite:workmate.db", migrations).build())
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
