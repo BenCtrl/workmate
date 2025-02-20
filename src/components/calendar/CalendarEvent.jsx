7e43e3f065a2d878581fd96ec1b28f4f09b7461c
Ben Cartwright <bcbenc5@hotmail.co.uk>
2025-02-20 00:57:13 +0000

    Implement content & logic from CalenderEventEditor into CalendarEvent and delete CalendarEventEditor component

--- src/components/calendar/CalendarEvent.jsx
+++ src/components/calendar/CalendarEvent.jsx
@@ -6,11 +6,24 @@ import { Save, Trash, X } from '../Icons';
 import { AppSettingsContext } from '../../App';
 
 import database from '../../database/database';
-import CalendarEventEditor from './CalendarEventEditor';
 
 const CalendarEvent = ({event, fetchEvents}) => {
   const SETTINGS = useContext(AppSettingsContext).appSettings;
   const [updatingEvent, setUpdatingEvent] = useState(false);
+  const [eventTitle, setEventTitle] = useState(event.title);
+
+  const updateEvent = async () => {
+    try {
+      await database.execute('UPDATE events SET title = $1 WHERE id = $2', [eventTitle, event.id]);
+
+      info(`Updated event '${eventTitle}' with ID '${event.id}'`);
+
+      fetchEvents();
+      setUpdatingEvent((state) => !state);
+    } catch(error) {
+      console.error(`Error while updating event with ID '${event.id}': ${error}`);
+    }
+  }
 
   const deleteEvent = async (id) => {
     try {
@@ -25,16 +38,34 @@ const CalendarEvent = ({event, fetchEvents}) => {
 
   return (
     <li key={event.id} className={`event ${updatingEvent ? 'updating' : ''}`}>
-      {updatingEvent ?
-        <CalendarEventEditor setUpdatingEvent={setUpdatingEvent} fetchEvents={fetchEvents} event={event} />
+      <span className="event-details">
+      <span className="event-timestamp">{new Date(event.event_timestamp).toLocaleTimeString([], {timeStyle: 'short'})}</span>
+        {updatingEvent ?
+        <input
+          className='inline-title-input'
+          autoFocus
+          value={eventTitle}
+          onChange={(changeEvent) => {setEventTitle(changeEvent.target.value)}}
+          type='text'
+          onKeyDown={(keyEvent) => {
+            if (keyEvent.code === "Escape") {
+              setUpdatingEvent((state) => !state);
+              setEventTitle(event.title);
+            }
+            keyEvent.code === "Enter" && updateEvent();
+          }}
+        />
         :
-        <>
-          <span className="event-details">
-          <span className="event-timestamp">{new Date(event.event_timestamp).toLocaleTimeString([], {timeStyle: 'short'})}</span>
-            <span title={`${SETTINGS.TOOLTIPS ? 'Edit event title':''}`} className="event-title" onClick={() => {setUpdatingEvent((state) => !state)}}>{event.title}</span>
-          </span>
-          <DeleteConfirmButton className="event-button mini" onClick={() => {deleteEvent(event.id)}} children={<Trash />} toolTip="Delete event" />
-        </>
+        <span title={`${SETTINGS.TOOLTIPS ? 'Edit event title':''}`} className="event-title" onClick={() => {setUpdatingEvent((state) => !state)}}>{event.title}</span>
+        }
+      </span>
+      {updatingEvent ?
+      <ButtonGroup>
+        <Button className='event-button mini' onClick={() => {setUpdatingEvent((state) => !state)}} children={<X />} toolTip="Cancel edit"/>
+        <Button className='event-button mini' onClick={() => {updateEvent()}} children={<Save />} toolTip="Save event"/>
+      </ButtonGroup>
+      :
+      <DeleteConfirmButton className="event-button mini" onClick={() => {deleteEvent(event.id)}} children={<Trash />} toolTip="Delete event" />
       }
     </li>
   )
