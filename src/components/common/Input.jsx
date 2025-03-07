1bdfd2fa0dec1a45a6e83109784efbc7b387e61b
Ben Cartwright <bcbenc5@hotmail.co.uk>
2025-03-07 10:46:04 +0000

    Implement autocomplete attribute to Input component with fixed value of off

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({id, name, placeholder, value, onChange, icon, className}) => {
   return (
     <div className={`text-input ${className}`}>
       {icon && icon}
-      <input id={id} name={name} placeholder={placeholder} value={value} onChange={onChange}/>
+      <input id={id} name={name} placeholder={placeholder} value={value} onChange={onChange} autoComplete='off'/>
     </div>
   )
 }
