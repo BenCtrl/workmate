7201bb898a28e20f719c3b67ae94b5007f6acc3d
Ben Cartwright <bcbenc5@hotmail.co.uk>
2025-03-17 20:17:26 +0000

    Revisit styling of "Last Edited" field for page list items

--- src/components/pages/PageListItem.jsx
+++ src/components/pages/PageListItem.jsx
@@ -9,7 +9,7 @@ const PageListItem = ({page, deletePage}) => {
     <li className="page-list-item">
       <NavLink to={`/pages/editor/${page.id}`}>
         <span className="page-list-item-title"><b>{page.title}</b></span>
-        <span className="page-list-item-date-edited"><span>Last Edited: </span>{new Date(page.edited_timestamp).toLocaleDateString()} | {new Date(page.edited_timestamp).toLocaleTimeString([], {timeStyle: 'short'})}</span>
+        <span className="page-list-item-date-edited">Last Edited: {new Date(page.edited_timestamp).toLocaleDateString()} | {new Date(page.edited_timestamp).toLocaleTimeString([], {timeStyle: 'short'})}</span>
       </NavLink>
       <DeleteConfirmButton className="page-list-item-options" onClick={() => {deletePage(page)}} children={<IconTrash />} toolTip="Delete Page" />
     </li>
